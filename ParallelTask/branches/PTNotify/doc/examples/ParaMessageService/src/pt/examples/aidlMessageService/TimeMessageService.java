e03b29ab3e14848b1c37019bcd948215ba1901f4
mmin779 <mmin779@08b7a59c-0210-0410-8289-8f00336d13cc>
2013-10-08 04:50:31 +0000

    Initial import.

---
+++ ParallelTask/branches/PTNotify/doc/examples/ParaMessageService/src/pt/examples/aidlMessageService/TimeMessageService.java
@@ -0,0 +1,18 @@
+package pt.examples.aidlMessageService;
+
+import android.os.RemoteException;
+
+public class TimeMessageService extends IRemoteMessageService.Stub {
+
+	private final AIDLMessageService service;
+	
+	public TimeMessageService(AIDLMessageService service) {
+		this.service = service;
+	}
+	
+	@Override
+	public String getMessage() throws RemoteException {
+		return service.getStringForRemoteService();
+	}
+
+}
