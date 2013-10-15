523cf556e980b677f44bd60ee196741eeb61cd7d
mmin779 <mmin779@08b7a59c-0210-0410-8289-8f00336d13cc>
2013-10-15 07:21:43 +0000

    add a test button.
    1. For a local service without <android:process=":process_name"> specified, it will run in the main thread of its hosting process, i.e. the UI thread of the application’s main process.
    So methods like onCreate, onStart, onDestroy are all run in the UI thread. Therefore, we can propose a Toast in these methods.
    It should spawn its own thread in which to do time-consuming work, and ParaTask can do that job. In this situation, ParaTask’s slot handling thread is just the UI thread of the app.
    
    2. For a local service specified <android:process=":process_name">, it will run in its own process. That process has its own UI thread with a running Looper, so a Toast proposed in the service’s onCreate or onStart methods won’t cause an exception as follows:
    
    java.lang.RuntimeException: Can't create handler inside thread that has not called Looper.prepare()
    
    If we use ParaTask in such a service, the slot handling thread will be the UI thread of the service process, and we can propose toasts in that UI thread.
    
    But why does the toast look like running in the app’s main thread? Yes, just looks like. Not really. A toast can pop up at the desktop, even if there is no Activity running at foreground. So toast is a special kind of UI widget, which can show up on top of all activities.

--- ParallelTask/branches/PTNotify/doc/examples/ParaService/src/pt/examples/paraservice/MainActivity.java
+++ ParallelTask/branches/PTNotify/doc/examples/ParaService/src/pt/examples/paraservice/MainActivity.java
@@ -2,10 +2,14 @@ package pt.examples.paraservice;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private final static String LOG_TAG = MainActivity.class.getCanonicalName();
@@ -14,12 +18,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Log.i(LOG_TAG, "ParaTask onCreate");
+		
+		Button test = (Button) findViewById(R.id.button3);
+		test.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(LOG_TAG, "ParaTask test button3 onClick");
+			}
+		});
 	}
 
 	// Start the  service
 	public void startNewService(View view) {
 		Log.i(LOG_TAG, "ParaTask startNewService");
 		startService(new Intent(this, ParaService.class));
+		/*
+		//final View v = view;
+		final Context c = getBaseContext();
+		
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Autod-generated method stub
+				Toast.makeText(c, "Complete!", Toast.LENGTH_LONG).show();
+			}});
+		t.start();
+		*/
 	}
 
 	// Stop the  service
