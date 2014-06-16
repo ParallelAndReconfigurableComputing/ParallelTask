4cf808565ebb9cbb211bbf941c599c314e47015d
mmin779 <mmin779@08b7a59c-0210-0410-8289-8f00336d13cc>
2014-06-16 14:40:17 +0000

    clone another benchmarks runner project for PTJava

---
+++ ParallelTask/branches/ParaTaskWithLambda/PTJavaBenchmarksRunner/src/main/java/org/sample/MyBenchmarkThread10.java
@@ -0,0 +1,29 @@
+package org.sample;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import pt.benchmarks.wrapper.ParaTaskWrapper;
+import pt.benchmarks.wrapper.VarianceWrapper;
+
+
+@State(Scope.Thread)
+public class MyBenchmarkThread10 {
+
+    double[] population;
+
+    @Setup
+    public void init() {
+        population = VarianceWrapper.generatePopulation(VarianceConfig.POPULATION_SIZE);
+        ParaTaskWrapper.setThreadPoolSize(10);
+    }
+    
+    @GenerateMicroBenchmark
+    public double testVarianceParaTaskWithLambda() {
+        return VarianceWrapper.varianceParaTaskWithLambda(population);
+    }
+    
+
+}
