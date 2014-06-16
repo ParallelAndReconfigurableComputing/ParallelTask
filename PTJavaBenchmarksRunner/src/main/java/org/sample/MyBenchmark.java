513cad0e1523d089de46c7cc8ca100c1e1547fed
mmin779 <mmin779@08b7a59c-0210-0410-8289-8f00336d13cc>
2014-06-16 14:40:17 +0000

    clone another benchmarks runner project for PTJava

---
+++ PTJavaBenchmarksRunner/src/main/java/org/sample/MyBenchmark.java
@@ -0,0 +1,42 @@
+package org.sample;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import pt.benchmarks.wrapper.VarianceWrapper;
+
+
+@State(Scope.Thread)
+public class MyBenchmark {
+
+    double[] population;
+
+    @Setup
+    public void init() {
+        population = VarianceWrapper.generatePopulation(VarianceConfig.POPULATION_SIZE);
+    }
+
+    @GenerateMicroBenchmark
+    public double testVarianceImperative() {
+        return VarianceWrapper.varianceImperative(population);
+    }
+
+    @GenerateMicroBenchmark
+    public double testVarianceStreams() {
+        return VarianceWrapper.varianceStreams(population);
+    }
+
+    @GenerateMicroBenchmark
+    public double testVarianceForkJoin() {
+        return VarianceWrapper.varianceForkJoin(population);
+    }
+    
+    @GenerateMicroBenchmark
+    public double testVarianceParaTaskWithLambda() {
+        return VarianceWrapper.varianceParaTaskWithLambda(population);
+    }
+    
+
+}
