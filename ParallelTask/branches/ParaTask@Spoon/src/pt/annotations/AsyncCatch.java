64e0266c221a83e9d0056a27c68f8a56306d1c07
mmin779 <mmin779@08b7a59c-0210-0410-8289-8f00336d13cc>
2014-06-27 09:14:07 +0000

    add ParaTask Annotations and examples, and the Spoon version 1.4 source code

---
+++ ParallelTask/branches/ParaTask@Spoon/src/pt/annotations/AsyncCatch.java
@@ -0,0 +1,12 @@
+package pt.annotations;
+
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
+
+@Target(LOCAL_VARIABLE)
+public @interface AsyncCatch {
+    Class throwable();
+    String handler();
+}
