39e17c42d15e3f8b33034997a81fb1150f61db64
mmeh012 <mmeh012@08b7a59c-0210-0410-8289-8f00336d13cc>
2014-09-17 01:44:22 +0000

    Added documented code for ParaTask

---
+++ ParallelTask/branches/MostafaMehrabi/ParaTaskLambdaJava8/src/pt/runtime/TaskpoolMixedScheduling.java
@@ -0,0 +1,192 @@
+/*
+ *  Copyright (C) 2010 Nasser Giacaman, Oliver Sinnen
+ *
+ *  This file is part of Parallel Task.
+ *
+ *  Parallel Task is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at 
+ *  your option) any later version.
+ *
+ *  Parallel Task is distributed in the hope that it will be useful, 
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+ *  Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along 
+ *  with Parallel Task. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pt.runtime;
+
+import java.util.AbstractQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.PriorityBlockingQueue;
+
+import pt.queues.FifoLifoQueue;
+
+/**
+ * The mixed scheduling policy is a combination of <code>Work Stealing</code> and <code>Work Sharing</code> policies,
+ * such that when enqueuing a task, if the enqueuing thread is a <i>worker thread</i>, the <code><b>Work Stealing</b></code>
+ * policy is used, and if the enqueuing thread is a <i>non-worker thread</i>, the <code><b>Work Sharing</b></code> policy is
+ * used!
+ * <br><br>
+ * A worker thread potentially prefers to execute tasks from its own <code>localOneOffTask</code> queue before helping with 
+ * the global shared queue!
+ * 
+ * @author Mostafa Mehrabi
+ * @since  14/9/2014
+ * */
+public class TaskpoolMixedScheduling extends AbstractTaskPool {
+	
+	/**
+	 * When enqueuing tasks under the <code>Mixed Scheduling</code> policy, if a task <b>cannot</b> be executed 
+	 * by arbitrary threads, it will be enqueued to the <code>privateQueue</code> of the thread in charge of
+	 * executing it. 
+	 * <br><br>
+	 * However, if the task can be executed by arbitrary threads it will be enqueued to the <code>mixedMultiTask</code>
+	 * queue if it is a <code>TaskIDGroup</code>, otherwise it will be enqueued to the <code>mixedOneOffTask</code> queue.
+	 * 
+	 * @author Mostafa Mehrabi
+	 * @sice   14/9/2014
+	 * */
+	@Override
+	protected void enqueueReadyTask(TaskID<?> taskID) {
+		//if the task cannot be executed on arbitrary threads, regardless of being
+		//a TaskIDGroup or a OneOff task, it will be enqueued to a private queue
+		if (taskID.getExecuteOnThread() != ParaTaskHelper.ANY_THREAD_TASK){
+			privateQueues.get(taskID.getExecuteOnThread()).add(taskID);
+		}
+		
+		//if the task can be executed on arbitrary threads, and is a group task
+		else if (taskID instanceof TaskIDGroup){
+			mixedMultiTaskqueue.addGlobal(taskID);
+		}
+		
+		//if the task can be executed on arbitrary threads, and is a one off task
+		else{
+			mixedOneoffTaskqueue.addGlobal(taskID);
+		}
+		
+	}
+	
+	/**
+	 * This method polls a new task for the current thread that is requesting for a new task under the 
+	 * <code>Mixed Scheduling</code> policy. This method should only be called by <code>Worker Threads</code>. 
+	 * <br><br>
+	 * Once a worker thread requests for a new task, this method checks if the thread is a <code>Multi Task</code>
+	 * thread. If so, then the <code>privateQueue</code> of that thread will be checked. In case there is a 
+	 * task found which passes the preliminary execution attempt successfully, the task is passed to the thread to execute.
+	 * <br><br>
+	 * However, if the private queue of the <code>Multi Task</code> worker thread does not have any executable tasks, then 
+	 * the method will check the <code>mixedMultiTask</code> queue. Every multi-task in this queue will be expanded into its
+	 * sub-tasks, and the sub-tasks will be enqueue as <code>ready-to-execute</code> tasks, but the thread will temporarily 
+	 * return <b>without</b> a task for this time, waiting for later chances. 
+	 * <br><br>
+	 * If the current worker thread is not a <code>Multi Task</code> worker thread, then the method checks the 
+	 * <code>mixedOneOffTask</code> queue. For a given task inside this queue, if the thread is allowed to execute it, the 
+	 * task will be checked with preliminary execution attempt. If the attempt is successful, the task will be sent to thread
+	 * to execute. However, if the thread is not allowed to execute the task, the task will be enqueued to the <code>privateQueue</code>
+	 * of the thread in charge of executing it.This process will continue until either a task is found for the thread, or all
+	 * tasks inside the <code>mixedOneOffTask</code> queue are polled and enqueued into <code>privateQueues</code>.
+	 * 
+	 *@author Mostafa Mehrabi
+	 *@since  14/9/2014
+	 * */
+	@Override
+	public TaskID workerPollNextTask() {
+		
+		WorkerThread wt = (WorkerThread) Thread.currentThread();
+		int workerID = wt.getThreadID();
+		
+		TaskID next = null;
+		
+		if (wt.isMultiTaskWorker()) {
+			next = privateQueues.get(workerID).poll();
+			
+			while (next != null) {
+				
+				//-- attempt to execute this task
+				if (next.executeAttempt()) {
+					//-- no cancel attempt was successful so far, therefore may execute this task
+					return next;
+				} else {
+					//-- task was successfully cancelled beforehand, therefore grab another task
+					next.enqueueSlots(true);	//-- task is considered complete, so execute slots
+					//-- TODO maybe should not execute slots for cancelled tasks, just the completedSlot() ?? 
+				}
+				
+				next = privateQueues.get(workerID).poll();
+			}
+			
+			// If the private queue of this multi-task does not have an executable task...
+			// get task from mixed-schedule queue.. if no suitable task found, return null
+			while ((next = mixedMultiTaskqueue.poll()) != null) {
+				
+				// expand multi task
+				int count = next.getCount();
+				int currentMultiTaskThreadPool = ThreadPool.getMultiTaskThreadPoolSize();
+				Task taskinfo = next.getTaskInfo();
+
+				// indicate this is a sub task
+				taskinfo.setSubTask(true);
+				
+				for (int i = 0; i < count; i++) {
+					TaskID taskID = new TaskID(taskinfo);
+					
+					taskID.setRelativeID(i);
+					taskID.setExecuteOnThread(i%currentMultiTaskThreadPool);
+					
+					taskID.setSubTask(true);
+					
+					taskID.setPartOfGroup(((TaskIDGroup)next));
+					((TaskIDGroup)next).add(taskID);
+					enqueueReadyTask(taskID);
+					
+				}
+				((TaskIDGroup)next).setExpanded(true);
+			}
+		}else {
+			// If the worker thread is not a multi-task thread look into the one off mixed queue...
+			// get task from mixed-schedule queue.. if no suitable task found, return null
+			while ((next = mixedOneoffTaskqueue.poll()) != null) {
+				int savedFor = next.getExecuteOnThread();
+				
+				// check if this task is saved for another worker thread
+				if (savedFor == ParaTaskHelper.ANY_THREAD_TASK || savedFor == workerID) {
+					if (next.executeAttempt()) {
+						return next;
+					} else {
+						//-- task was successfully cancelled beforehand, therefore grab another task
+						next.enqueueSlots(true);	//-- task is considered complete, so execute slots
+						//-- TODO maybe should not execute slots for cancelled tasks, just the completedSlot() ?? 
+					}
+				} else {
+					
+					
+		  			//privateQueues[savedFor].add(next);
+					privateQueues.get(savedFor).add(next);
+				}
+			}
+		}
+		
+		
+		return null;
+	}
+
+	
+	@Override
+	protected void initialise() {
+	
+		//For multi task
+		mixedMultiTaskqueue = new FifoLifoQueue<TaskID<?>>();
+		
+		privateQueues = new ArrayList<AbstractQueue<TaskID<?>>>();
+		
+		//For one-off task
+		mixedOneoffTaskqueue = new FifoLifoQueue<TaskID<?>>();
+		
+		initialiseWorkerThreads();
+	}
+}
---
+++ ParallelTask/branches/MostafaMehrabi/ParaTaskLambdaJava8/src/pt/runtime/ThreadPool.java
@@ -0,0 +1,401 @@
+/**
+ * 
+ */
+package pt.runtime;
+
+import java.util.AbstractQueue;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
+
+import pt.runtime.TaskID;
+import pt.runtime.Taskpool;
+import pt.runtime.WorkerThread;
+import pt.runtime.ParaTask.ThreadPoolType;
+
+
+/**
+ * @author Kingsley
+ * 
+ * @since 01/10/2013
+ * Remove all the unnecessary code before merging.
+ */
+public class ThreadPool {
+
+	/**
+	 * Indicates the size of the thread pool, and is automatically determined according to the 
+	 * number of <code>CPU cores</code> at runtime. 
+	 * 
+	 * @author Mostafa Mehrabi
+	 * @since  15/9/2014
+	 * */
+	private static int poolSize = -1;
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 18/05/2013
+	 * 
+	 * Use the field of "oneoffTaskThreadPoolSize" to trace how many one-off task
+	 * worker threads should EXACTLY exist in the one-off task thread pool
+	 * Use the field of "multiTaskThreadPoolSize" to trace how many one-off task
+	 * worker threads should EXACTLY exist in the one-off task thread pool
+	 * 
+	 * @since 31/05/2013
+	 * Need a lock to make sure that all the operations on the thread pool collection 
+	 * are atomic.
+	 * 
+	 * */
+	private static int multiTaskThreadPoolSize;
+	
+	private static int oneoffTaskThreadPoolSize;
+
+	private final static ReentrantLock reentrantLock = new ReentrantLock();
+	
+	/**
+     * 
+     * 
+	 * The pools are <code>TreeMaps</code>, the key is the thread id.
+	 * The benefit with <code>TreeMaps</code> is that they are already sorted, so 
+	 * we can get the worker thread who owns the highest <code>threadID</code>, or get the worker thread just simply using 
+	 * its <code>threadID</code> as an index.
+	 * <br><br>
+	 * One-off task worker thread does not need to be sorted and synchronized.
+	 * Using HashMap instead.
+	 * 
+	 * @author Kingsley
+	 * @since 16/05/2013
+	 * 
+	 */
+	private static SortedMap<Integer, WorkerThread> multiTaskWorkers = Collections.synchronizedSortedMap(new TreeMap<Integer, WorkerThread>());
+	
+	private static Map<Integer, WorkerThread> oneoffTaskWorkers = new HashMap<Integer, WorkerThread>();
+
+	
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 18/05/2013
+	 * 
+	 * Used to maintain the global id
+	 * 
+	 * */
+	private static int globalID = 0;
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 11/05/2013
+	 * 
+	 * This is used to enqueue fake task
+	 * The fake task is used to kill threads.
+	 * */
+	private static Taskpool taskpool;
+
+	static {
+		if (poolSize < 0) {
+			poolSize = Runtime.getRuntime().availableProcessors();
+		}
+	}
+
+	/**
+	 * 
+	 * @Author Kingsley
+	 * @since 25/04/2013
+	 * Initialize worker threads here
+	 * 
+	 * */
+	
+	protected static void initialize(Taskpool taskpool) {
+		ThreadPool.taskpool = taskpool;
+		initializeWorkerThreads(taskpool);
+	}
+	
+	/**
+	 * This method initializes the <code>Worker Threads</code> that are going to execute tasks
+	 * in this instance of <code>Thread Pool</code>. In the first step, this method creates as 
+	 * many <code>Multi-task Threads</code> as half of the <code>CPU cores</code> at runtime 
+	 * (i.e. half of <code>poolSize</code>). 
+	 * <br><br>
+	 *Then the <code>Mutli-task Threads</code> are granted <b>maximum</b> priority, and are allowed
+	 *to be active as <b>daemon</b> threads if required. Then the <code>Mutli-task Threads</code> are 
+	 *added to a <code><i>sorted tree map</i></code>, where their <code>globalID</code> is used as 
+	 *the key! Finally there is a <code>privateQueue</code> created and associated to each <code>Mutli-task Thread</code>.
+	 *The <code>privateQueues</code> are collected in a <b>list</b> of <code>privateQueues</code>, where the order of
+	 *queues is same as the order of threads created. 
+	 *<br><br> 
+	 *Threads are given <code>globalIDs</code> which indicates their position in the thread pool. <code>Multi-task Threads</code>
+	 *are also given a <code>localID</code> or <code>multiTaskWorkerID</code> which indicates the position of 
+	 *<code>Multi-task Threads</code> with respect to each other.
+	 *<br><br>
+	 *After the <code>Multi-task Threads</code> are created, the other half of <code>poolSize</code> (i.e. No. of CPU-cores) is
+	 *specified to <code>OneOff-task</code> threads. There will be <code>globalID</code>s associated to <code>OneOff-task</code>
+	 *threads as both <u>local</u> and <u>global</u> IDs, because <code>OneOff-tasks</code> threads don't really need a
+	 *local IDs. Same as <code>Multi-task</code> threads, <code>OneOff-task</code> threads are given <b>maximum</b> priority, 
+	 *and are allowed to be active as <b>daemon</b> threads if required. <code>OneOff-task</code> threads are then added to
+	 *a <i>Map</i> collection which maps a worker thread to its <code>globalID</code>. Moreover, there is one 
+	 *<code>localOneOffTask</code> queue associated to each <code>OneOff-task</code> thread. The indices of local queues for both 
+	 *worker thread types starts from 0.
+	 * 
+	 * @Author Kingsley
+	 * @author Mostafa Mehrabi
+	 * 
+	 * @since 26/04/2013
+	 * @since 15/9/2014
+	 * */
+	private static void initializeWorkerThreads(Taskpool taskpool) {
+		
+		int multiTaskWorkerID = 0;
+		
+		int multiTaskThreadPoolSize = poolSize/2;
+		
+		for (int i = 0; i < poolSize; i++, globalID++) {
+			if (multiTaskWorkerID < multiTaskThreadPoolSize) {
+				WorkerThread workers = new WorkerThread(globalID, multiTaskWorkerID, taskpool, true);
+				workers.setPriority(Thread.MAX_PRIORITY);
+				workers.setDaemon(true);
+
+				multiTaskWorkers.put(globalID, workers);
+				
+				multiTaskWorkerID++;
+				
+				/**
+				 * 
+				 * @author Kingsley
+				 * @since : 18/05/2013
+				 * No matter what scheduling policy is, private task queues
+				 * are always required. Before putting something inside, the collection
+				 * of the queues must have already been initialized. 
+				 * 
+				 * */
+				List<AbstractQueue<TaskID<?>>> privateTaskQueues = taskpool.getPrivateTaskQueues();
+				privateTaskQueues.add(new PriorityBlockingQueue<TaskID<?>>(
+						AbstractTaskPool.INITIAL_QUEUE_CAPACITY,
+						AbstractTaskPool.FIFO_TaskID_Comparator));
+				
+				workers.start();
+			}else {
+			
+				/**
+				 * 
+				 * @Auther : Kingsley
+				 * @since : 02/05/2013
+				 * One-off task threads do not need local thread ID (multiTaskID). 
+				 * Simply use its <code>globalID</code> for both local and global IDs.
+				 * 
+				 * */
+				WorkerThread workers = new WorkerThread(globalID, globalID, taskpool, false);
+				workers.setPriority(Thread.MAX_PRIORITY);
+				workers.setDaemon(true);
+				oneoffTaskWorkers.put(globalID, workers);
+				Map<Integer, LinkedBlockingDeque<TaskID<?>>> localOneoffTaskQueues = taskpool.getLocalOneoffTaskQueues();
+				if (null != localOneoffTaskQueues) {
+					localOneoffTaskQueues.put(globalID, new LinkedBlockingDeque<TaskID<?>>());
+				}
+
+				workers.start();
+			}
+		}
+		multiTaskThreadPoolSize = multiTaskWorkers.size();
+		oneoffTaskThreadPoolSize = oneoffTaskWorkers.size();
+	}
+	
+	//? How should I return the pool size ?
+	//Maybe multi task thread pool size + one-off task thread pool size
+	protected static int getPoolSize(ThreadPoolType threadPoolType) {
+		switch (threadPoolType) {
+		case ONEOFF:
+			return oneoffTaskThreadPoolSize;
+		case MULTI:
+			return multiTaskThreadPoolSize;
+		default:
+			return multiTaskThreadPoolSize + oneoffTaskThreadPoolSize;
+		}
+	}
+
+	/**
+	 * 
+	 * This method sets the size of the <code>Thread Pool</code> and depending on the type
+	 * of the <code>Thread Pool</code> it takes further required actions as a result of 
+	 * the change. The further actions take place in {@link #adjustThreadPool(ThreadPoolType, int)}.
+	 * 
+	 * @author Mostafa Mehrabi
+	 * @since  15/9/2014
+	 * */
+	
+	protected static void setPoolSize(ThreadPoolType threadPoolType, int poolSize) {
+		ThreadPool.poolSize = poolSize;
+		adjustThreadPool(threadPoolType, poolSize);
+	}
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 18/05/2013
+	 * 
+	 * Change the getter methods, get the pool size directly from returning the collection size. 
+	 * */
+	
+	protected static int getMultiTaskThreadPoolSize() {
+		return multiTaskWorkers.size();
+	}
+	
+	protected static int getOneoffTaskThreadPoolSize() {
+		return oneoffTaskWorkers.size();
+	}
+
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 18/05/2013
+	 * 
+	 * Adjust the pool size according to the parameter.
+	 * Only called after user called setPoolSize(int poolSize);
+	 * setMultiTaskThreadPoolSize(int multiTaskThreadPoolSize) or 
+	 * setOneoffTaskThreadPoolSize(int oneoffTaskThreadPoolSize)
+	 * 
+	 * @param ThreadPoolType Indicate different thread pool 
+	 * @param newSize
+	 * @return 
+	 * */
+	private static void adjustThreadPool(ThreadPoolType type, int newSize) {
+		switch (type) {
+		case ALL:
+			adjustMultiTaskThreadPool(newSize);
+			adjustOneoffThreadPool(newSize);
+			break;
+		case ONEOFF:
+			adjustOneoffThreadPool(newSize);
+			break;
+		case MULTI:
+			adjustMultiTaskThreadPool(newSize);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 18/05/2013
+	 * @param newSize
+	 * @return 
+	 * 
+	 * Adjust the pool size for multi task thread pool
+	 * Work on this part later.
+	 * */
+	private static void adjustMultiTaskThreadPool(int newSize){
+		
+	}
+	
+	/**
+	 * Adjust the pool size for one-off task thread pool. If the new size is smaller than 
+	 * the original size of <code>OneOff-task</code> thread pool, then all threads in <code>OneOff-task</code>
+	 * thread pool will get poisoned. However, because <code>lottoNum</code> is set to the number of 
+	 * <i>redundant threads</i>, only the redundant threads will be removed. 
+	 * <br><br>
+	 * If the new size is bigger than the original size of <code>OneOff-task</code> thread pool, then there will
+	 * be as many <code>OneOff-task</code> worker threads added to the <code>OneOff-task</code> thread pool as it
+	 * is required to fill the new capacity. Threads will be granted <b>maximum</b> priority, and will be allowed 
+	 * to behave as <b>daemon</b> threads if required. The threads will be added to the <code>OneOff-task</code>
+	 * thread pool, and their corresponding <code>localOneOffTask</code> queue will be added to the <code>Map</code>
+	 * collection. 
+	 *
+	 *@author Mostafa Mehrabi
+	 *@since 16/9/2014
+	 * 
+	 * */
+	private static void adjustOneoffThreadPool(int newSize){
+		if (oneoffTaskThreadPoolSize == newSize) {
+			return;
+		}else if (oneoffTaskThreadPoolSize > newSize) {
+			int diff = oneoffTaskThreadPoolSize - newSize;
+
+			LottoBox.setLotto(diff);
+			
+			reentrantLock.lock();
+			
+			for (Iterator<WorkerThread> iterator = oneoffTaskWorkers.values().iterator(); iterator.hasNext();) {
+				WorkerThread workerThread = (WorkerThread) iterator.next();
+				workerThread.requireCancel(true);
+			}
+			
+			reentrantLock.unlock();
+			
+			oneoffTaskThreadPoolSize = oneoffTaskThreadPoolSize - diff;
+		}else {
+			int diff = newSize - oneoffTaskThreadPoolSize;
+			
+			for (int i = 0; i < diff; i++, globalID++) {
+				WorkerThread workers = new WorkerThread(globalID, globalID, taskpool, false);
+				workers.setPriority(Thread.MAX_PRIORITY);
+				workers.setDaemon(true);
+
+				oneoffTaskWorkers.put(globalID, workers);
+				Map<Integer, LinkedBlockingDeque<TaskID<?>>> localOneoffTaskQueues = taskpool.getLocalOneoffTaskQueues();
+				if (null != localOneoffTaskQueues) {
+					localOneoffTaskQueues.put(globalID, new LinkedBlockingDeque<TaskID<?>>());
+				}
+				workers.start();
+			}
+			
+			oneoffTaskThreadPoolSize = oneoffTaskWorkers.size();
+		}
+	}
+	
+	/**
+	 * 
+	 * @author Kingsley
+	 * @since 16/05/2013
+	 * 
+	 * Before worker thread really stop working, give it a chance to tell the threadpool
+	 * its thread type and its thread id. Then the threadpool can remove it from the 
+	 * corresponding thread pool
+	 * 
+	 * @since 23/05/2013
+	 * Do not re-allocate tasks AND let other works keep stealing
+	 * Acquire a lock before accessing the one-off task worker thread pool
+	 * */
+	protected static void lastWords(boolean isMultiTaskWorker, int threadID){
+		if (isMultiTaskWorker) {
+			
+		}else {
+			reentrantLock.lock();
+			//shouldn't we remove their taskqueues as well?
+			oneoffTaskWorkers.remove(threadID);
+			
+			reentrantLock.unlock();
+		}
+	}
+
+	/**
+	 * 
+	 * @Author Kingsley
+	 * @since 18/06/2013
+	 * @return int
+	 * 
+	 * Returns the approximate number of threads that are actively executing tasks.
+	 * 
+	 * */
+	public static int getActiveCount(ThreadPoolType type) {
+		switch (type) {
+		case ONEOFF:
+			return oneoffTaskWorkers.size();
+		case MULTI:
+			return multiTaskWorkers.size();
+		default:
+			return oneoffTaskWorkers.size()+multiTaskWorkers.size();
+		}
+	}
+ }
+
+
