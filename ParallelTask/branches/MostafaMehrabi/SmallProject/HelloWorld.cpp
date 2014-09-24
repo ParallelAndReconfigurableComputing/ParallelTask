cd3390b0e390f0f3883d038ee4c91964b15db54d
mmeh012 <mmeh012@08b7a59c-0210-0410-8289-8f00336d13cc>
2014-09-24 22:26:40 +0000

    some minor changes to HlloWorld.cpp in SmallProject

--- ParallelTask/branches/MostafaMehrabi/SmallProject/HelloWorld.cpp
+++ ParallelTask/branches/MostafaMehrabi/SmallProject/HelloWorld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
 
 using namespace std;
 
 int main (int argc, char* argv[]){
+	system("clear");
 	for (int i = 0; i < 10; i++)
-		cout << "hello World!";
+		cout << "hello World!" << endl;
 }
